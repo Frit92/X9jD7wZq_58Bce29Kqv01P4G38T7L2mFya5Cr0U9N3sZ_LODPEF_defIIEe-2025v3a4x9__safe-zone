33f6a9f37d646f52d355da157670a4baef064a54
Frit92 <ftegtmeier92@gmail.com>
2025-11-24 15:54:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // sw.js – Fritflix Feedback PWA Service Worker
-// Version: v1.1.2
+// Version: v1.1.3
 // Pfad: /X9jD7wZq_58Bce29Kqv01P4G38T7L2mFya5Cr0U9N3sZ_LODPEF_defIIEe-2025v3a4x9__safe-zone
 
-const CACHE_NAME = 'fritflix-feedback-v1.1.2';
+const CACHE_NAME = 'fritflix-feedback-v1.1.3';
 const BASE_PATH = '/X9jD7wZq_58Bce29Kqv01P4G38T7L2mFya5Cr0U9N3sZ_LODPEF_defIIEe-2025v3a4x9__safe-zone';
 
 const urlsToCache = [
@@ -104,5 +104,6 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
 
 
